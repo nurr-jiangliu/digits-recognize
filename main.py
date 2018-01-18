b876a93b988d457471f8d557aa9bdf171d02221d
JunJun-Love-Amber <254742378@qq.com>
2018-01-18 16:10:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ if __name__ == '__main__':
     print("redis:" + str(redis.ping()))
     svm = amber.svm.SVM(redis)
     Thread(target=svm.predict).start()
-    app.run(host="0.0.0.0", port=80, debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=8080, debug=False, threaded=True)
